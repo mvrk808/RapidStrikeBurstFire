3fae044377df737d34a17a10bdfa91ff7138ddf8
Matthijs Vermeij <matthijs@onzichtbarehelden.nl>
2018-03-14 21:02:11 +0100

    some cleaning up of the code, including the original burstCount value, and resetting the burstCount in FA mode (which made no sense because the FA mode doesn't take into account burstCount)

--- rapidstrike_burst_1.0.ino
+++ rapidstrike_burst_1.0.ino
@@ -17,7 +17,7 @@ const int fireModePin = 5;
 int sequence = 0;
 int pushCount = 0;
 int fireMode = 3 ; // Single, Burst, FA (1,2,3)
-int burstCount = 10; // Burst fire Count
+int burstCount = 0; // Burst fire Count
 int pusherPressedDown = 0;
 
 Bounce pusher = Bounce();
@@ -32,7 +32,7 @@ void setup() {
   pusher.attach(pusherPin);
   pusher.interval(5);
   
-  //pinMode(fireModePin, INPUT_PULLUP);
+  //pinMode(fireModePin, INPUT_PULLUP); // fireModePin is currently still pseudo code.
   digitalWrite(relayPin, HIGH); 
   pinMode(relayPin, OUTPUT);
   //Serial.begin(9600);
@@ -47,7 +47,7 @@ void loop() {
 
   // Determine firemode. Only when not in a sequence.
   if(sequence == 0) {
-      //fireMode = digitalRead(fireModePin);
+      //fireMode = digitalRead(fireModePin); //commented out until hardware is connected
       if(fireMode == 1) {
         burstCount = 1;
       } else if(fireMode == 2) {
@@ -55,7 +55,7 @@ void loop() {
       } 
       //determine whether trigger pull happened. Ignore trigger pull within sequence.
       if(trigger.fell()) {
-        //Serial.println(" trigger push" );
+        //Serial.println(" trigger push" ); //debugging line
         sequence = 1;
         digitalWrite(relayPin, LOW); // Start pushing.
       }
@@ -64,24 +64,25 @@ void loop() {
   // determine sequence behavior, based on fireMode. <3 == single or burst behavior.  3 == Full Auto
   if(fireMode < 3) {
       if(sequence == 1) {
+          
           //Pusher retracted
           if(pusher.fell()) {
             pushCount++;
           } 
-
+          
           //Check whether burst should end
-            if(pushCount >= burstCount) {
-              digitalWrite(relayPin, HIGH);
-              sequence = 0;
-              pushCount = 0;
-            }
+          if(pushCount >= burstCount) {
+            digitalWrite(relayPin, HIGH);
+            sequence = 0;
+            pushCount = 0;
+          }
       }
   } else if(fireMode == 3) {
     if(sequence == 1) {
+       //Check whether Full Auto sequence should end
        if(pusher.fell() && trigger.read() == 1) {
           digitalWrite(relayPin, HIGH);
           sequence = 0;
-          pushCount = 0;
        }
     }
   }
